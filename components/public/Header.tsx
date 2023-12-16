8ce8293d435c94c5bf445ac19eb9c9975d95f39f
nnawar706 <nnawar706@gmail.com>
2023-12-16 17:50:00 +0600

    header component

--- components/public/Header.tsx
+++ components/public/Header.tsx
@@ -1,5 +1,15 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export const Header = () => {
     return (
-        <p>Header</p>
+        <header className="w-full border-b">
+            <div className="wrapper flex items-center justify-between">
+                <Link href="/" className="w-36">
+                    <Image src="/assets/images/logo.svg" alt="Logo" width={128} height={38}/>
+                </Link>
+                <div className="flex w-32 justify-end gap-3"></div>
+            </div>
+        </header>
     )
 }
