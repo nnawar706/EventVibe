0935a573ddb9e17108182a39751ae5be8de6c41f
nnawar706 <nnawar706@gmail.com>
2023-12-17 14:40:25 +0600

    auth navs

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-
+      <section className="py-5 md:py-10 bg-primary-50 bg-dotted-pattern bg-contain"></section>
     </main>
   )
 }
--- components/public/Footer.tsx
+++ components/public/Footer.tsx
@@ -1,5 +1,16 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export const Footer = () => {
     return (
-        <p>Footer</p>
+        <footer className="border-t">
+            <div className="flex flex-col flex-between fkex-center wrapper gap-4 p-5
+            text-center sm:flex-row">
+                <Link href="/">
+                    <Image src="/assets/images/logo.svg" alt="logo" width={128} height={38}/>
+                </Link>
+                <p>EventVibe@2024. All Rights Reserved.</p>
+            </div>
+        </footer>
     )
 }
