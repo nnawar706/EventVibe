854be389e682fc1afd2f191a0126e85ace778513
nnawar706 <nnawar706@gmail.com>
2023-12-17 13:09:14 +0600

    toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from 'next/font/google'
 import {ClerkProvider} from "@clerk/nextjs";
 
 import './globals.css'
+import {Toaster} from "@/components/ui/toaster";
 
 const poppins = Poppins({
     subsets: ['latin'],
@@ -26,7 +27,10 @@ export default function RootLayout({
     return (
         <ClerkProvider>
             <html lang="en">
-                <body className={poppins.className}>{children}</body>
+                <body className={poppins.className}>
+                    {children}
+                    <Toaster />
+                </body>
             </html>
         </ClerkProvider>
     )
---
+++ lib/helper.ts
@@ -0,0 +1,14 @@
+// export default function callToast(
+//     toast: React.RefObject<Toast | null>,
+//     type: boolean | string,
+//     msg: string
+// ) {
+//     if (toast.current) {
+//         toast.current?.show({
+//             severity: type === "warn" ? "warn" : type ? "success" : "error",
+//             summary: type === "warn" ? "Warning" : type ? "Successful" : "Error",
+//             detail: msg,
+//             life: 3000,
+//         });
+//     }
+// }
