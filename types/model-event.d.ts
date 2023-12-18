e4ddac54656279ad6f9b7636228529f5e725885d
nnawar706 <nnawar706@gmail.com>
2023-12-18 12:56:35 +0600

    db-models

--- types/model-event.d.ts
+++ types/model-event.d.ts
@@ -1,8 +1,21 @@
-import { Document } from "mongoose"
+import { Document, Types } from "mongoose"
 
-export interface IEvent extends Document {
+export interface IEvent extends Event, Document {
     _id: string;
-    organizer: {}
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+interface Event {
+    organizer: Types.ObjectId;
+    category: Types.ObjectId;
     title: string;
-    description?: string
+    description?: string;
+    location?: string;
+    locationLat?: string;
+    locationLng?: string;
+    startDateTime: Date;
+    endDateTime: Date;
+    ticketPrice?: string;
+    url?: string;
 }
