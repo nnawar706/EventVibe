16c9743deec0dab9452ad4bf93ff41ac02c382b8
nnawar706 <nnawar706@gmail.com>
2024-01-02 18:30:18 +0600

    uploadthing

--- components/private/EventForm.tsx
+++ components/private/EventForm.tsx
@@ -3,7 +3,11 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
+import {useState} from "react"
+import Image from "next/image"
+import DatePicker from "react-datepicker"
 
+import "react-datepicker/dist/react-datepicker.css"
 import { Button } from "@/components/ui/button"
 import {Form, FormControl, FormField, FormItem, FormMessage} from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
@@ -11,11 +15,11 @@ import {eventFormSchemaValidator} from "@/lib/validations/eventFormValidator"
 import {EventFormProps} from "@/types/general"
 import { eventDefaultValues } from "@/constants"
 import Dropdown from "@/components/public/Dropdown"
-import FileUploader from "@/components/public/FileUploader";
-import {useState} from "react";
+import FileUploader from "@/components/public/FileUploader"
 
 const EventForm = ({ authId, type }: EventFormProps ) => {
     const [files, setFiles] = useState<File[]>([])
+    const [startDate, setStartDate] = useState(new Date())
     const initialValues = eventDefaultValues
 
     const form = useForm<z.infer<typeof eventFormSchemaValidator>>({
@@ -87,6 +91,60 @@ const EventForm = ({ authId, type }: EventFormProps ) => {
                                     onFieldChange={field.onChange}
                                     imageUrl={field.value}
                                     setFiles={setFiles}/>
+                                </FormControl>
+                                {/* <FormMessage /> */}
+                                {fieldState.error && <FormMessage>{fieldState.error.message}</FormMessage>}
+                            </FormItem>
+                        )}
+                    />
+                </div>
+
+                <div className="flex flex-col gap-5 md:flex-row">
+                    <FormField
+                        control={form.control}
+                        name="startDateTime"
+                        render={({ field, fieldState }) => (
+                            <FormItem className="w-full">
+                                <FormControl>
+                                    <div className="flex-center h-[54px] w-full overflow-hidden
+                                    rounded-full bg-grey-50 px-4 py-2">
+                                        <Image
+                                            src="/assets/icons/calendar.svg"
+                                            width={24}
+                                            height={24}
+                                            alt="calendar"
+                                        />
+                                        <p className="ml-3 whitespace-nowrap text-grey-600">From:</p>
+                                        <DatePicker
+                                            selected={field.value}
+                                            onChange={(date: Date) => field.onChange(date)}
+                                        />
+                                    </div>
+                                </FormControl>
+                                {/* <FormMessage /> */}
+                                {fieldState.error && <FormMessage>{fieldState.error.message}</FormMessage>}
+                            </FormItem>
+                        )}
+                    />
+                </div>
+
+                <div className="flex flex-col gap-5 md:flex-row">
+                    <FormField
+                        control={form.control}
+                        name="location"
+                        render={({ field, fieldState }) => (
+                            <FormItem className="w-full">
+                                <FormControl>
+                                    <div className="flex-center h-[54px] w-full overflow-hidden
+                                    rounded-full bg-grey-50 px-4 py-2">
+                                        <Image
+                                            src="/assets/icons/location-grey.svg"
+                                            width={24}
+                                            height={24}
+                                            alt="calendar"
+                                        />
+                                        <Input placeholder="Event Location" {...field} className="input-field" />
+                                    </div>
                                 </FormControl>
                                 {/* <FormMessage /> */}
                                 {fieldState.error && <FormMessage>{fieldState.error.message}</FormMessage>}
